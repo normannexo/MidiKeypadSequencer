a08a387c8eeabcfa471ace371208355f191b8ec1
Norman Nexo <normannexo@gmail.com>
2016-10-16 15:58:48 +0200

    Added StepSequence Class

---
+++ initsequences.h
@@ -0,0 +1,7 @@
+#ifndef initsequences_h
+#define initsequences_h
+
+int pat1_notes[16] = {60,60, 60, 60, 60, 60, 60, 60, 44, 60, 60, 60, 60, 60, 60, 60};
+boolean pat1_actives[16] = {true, false, true, false, true, true, false, true, true, true, true, true, true, true, true, true};
+
+#endif
